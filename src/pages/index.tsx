69b0817d9630bec60e9a5e23587bd0a7cbd8f1c6
Kelvin Costa <okelvincosta@gmail.com>
2026-01-30 19:09:53 -0300

    🎉 init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Button } from "@/components/ui/button";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,7 +14,67 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div
+    <>
+      <div className="bg-slate-950 h-screen text-white flex lg:items-center">
+        {/* Grid */}
+        <main className="w-full h-screen grid lg:items-center grid-cols-1 gap-6 lg:grid-cols-12">
+          <div className="h-[50vh] lg:h-auto lg:col-span-5 relative rounded-4xl">
+            <Image
+              src="/assets/cover-login.png"
+              width={578}
+              height={900}
+              alt=""
+              quality={90}
+              className=" max-h-[50vh] h-full lg:min-h-screen p-8 object-cover rounded-[45px] w-full"
+            />
+
+            <Image
+              src="/assets/logo-anikokoro.svg"
+              width={213}
+              height={68}
+              alt=""
+              priority
+              className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
+            />
+          </div>
+          <div className="lg:h-auto lg:col-span-7 flex lg:items-center pt-[7vh] justify-center h-[50vh]">
+            <div>
+              <h3>Boas vindas!</h3>
+              <p>Faça seu login ou acesse como visitante.</p>
+              <div className="flex flex-col gap-3 mt-10">
+                <Button
+                  size="lg"
+                  className=" bg-slate-800 rounded-sm !text-left max-w-[370px]"
+                >
+                  <Image
+                    src="/assets/icon-google.svg"
+                    width={32}
+                    height={32}
+                    alt=""
+                    className="mr-1"
+                  ></Image>
+                  Entrar com Google
+                </Button>
+                <Button
+                  size="lg"
+                  className=" bg-slate-800 rounded-sm !text-left max-w-[370px]"
+                >
+                  <Image
+                    src="/assets/icon-guest.svg"
+                    width={32}
+                    height={32}
+                    alt=""
+                    className="mr-1"
+                  ></Image>
+                  Acessar como visitante
+                </Button>
+              </div>
+            </div>
+          </div>
+        </main>
+        {/* Grid End*/}
+      </div>
+      {/* <div
       className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
     >
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -25,54 +86,9 @@ export default function Home() {
           height={20}
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the index.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs/pages/getting-started?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+        
       </main>
-    </div>
+    </div> */}
+    </>
   );
 }
